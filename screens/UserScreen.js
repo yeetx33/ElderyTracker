5de50cd94b816667879c638366f3b75b426db94b
Mohammad Khalil <moody.khalil511@gmail.com>
2024-05-05 21:46:04 +0300

    Revert "userScreen and main page added //khalil"
    This reverts commit b131464c55443f74c295ff94e4609fec1f05628f.

--- screens/UserScreen.js
+++ screens/UserScreen.js
@@ -1,42 +1,12 @@
-import React, { useState } from "react";
-import { Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import { COLORS, SIZES } from "../constents/theme";
-import NavBar from "../components/NavBar";
-import Home from "../components/Home";
-import Settings from "../components/Settings";
-import Notifications from "../components/Notifications";
+import React from "react";
+import { Text, View } from 'react-native';
 
 const UserScreen=({navigation}) => {
-    const [currentPage, setCurrentPage] = useState("home");
-    
-    onChangePageHandler = (state) => {
-      setCurrentPage(state);
-    }
-
     return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <SafeAreaView style={styles.container}>
-        {currentPage === 'settings' && <Settings />}
-        {currentPage === 'home' && <Home />}
-        {currentPage === 'notifications' && <Notifications />}
-      </SafeAreaView>
-
-      <NavBar fromWhere={currentPage} onChangePageHandler={onChangePageHandler}/>
-
-    </View>
+    <View style={{ flex: 1, justifyContent: 'center',
+     alignItems: 'center' }}>
+    <Text>UserScreen!</Text>
+  </View>
     );
 }
-
-const styles = StyleSheet.create({
-
-    container: {
-      flex: 1,
-      width: "100%",
-      flexDirection: "column",
-      marginTop: Platform.OS === "android" && StatusBar.currentHeight,
-      position: "relative",
-      backgroundColor: COLORS.primary,
-    }
-  })
-
 export default UserScreen;
